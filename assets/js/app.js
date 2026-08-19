a19486b092f0ce865dc15cd4895bf66f979a47ca
jevelynlabor <jevelyn@almondmarketing.co.uk>
2026-08-19 15:45:49 +0800

    Added blogs, corrected app.js for the menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,14 @@
-<script>
-  const btn = document.getElementById('hamburger');
-  const menu = document.getElementById('navLinks');
-  btn.addEventListener('click', () => {
-    const isOpen = menu.classList.toggle('open');
-    btn.classList.toggle('open', isOpen);
-    btn.setAttribute('aria-expanded', isOpen);
+const btn = document.getElementById('hamburger');
+const menu = document.getElementById('navLinks');
+btn.addEventListener('click', () => {
+  const isOpen = menu.classList.toggle('open');
+  btn.classList.toggle('open', isOpen);
+  btn.setAttribute('aria-expanded', isOpen);
+});
+menu.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('open');
+    btn.classList.remove('open');
+    btn.setAttribute('aria-expanded', false);
   });
-  menu.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', () => {
-      menu.classList.remove('open');
-      btn.classList.remove('open');
-      btn.setAttribute('aria-expanded', false);
-    });
-  });
-</script>
+});
